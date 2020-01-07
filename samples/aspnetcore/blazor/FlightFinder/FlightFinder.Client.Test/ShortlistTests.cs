9b9572d3f4be8052c9750f860c73c5da3d75923e
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-01-07 13:56:54 +0000

    Shortlist test in failing state

--- samples/aspnetcore/blazor/FlightFinder/FlightFinder.Client.Test/ShortlistTests.cs
+++ samples/aspnetcore/blazor/FlightFinder/FlightFinder.Client.Test/ShortlistTests.cs
@@ -2,7 +2,6 @@
 using FlightFinder.Client.Components;
 using FlightFinder.Client.Test.TestHelpers;
 using FlightFinder.Shared;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Testing;
 using System.Collections.Generic;
 using Xunit;
@@ -16,10 +15,7 @@ namespace FlightFinder.Client.Test
         [Fact]
         public void CanDisplayEmpty()
         {
-            var component = host.AddComponent<Shortlist>(new Dictionary<string, object>
-            {
-                [nameof(Shortlist.Itineraries)] = new List<Itinerary>()
-            });
+            var component = host.AddComponent<Shortlist>();
 
             Assert.Equal("Shortlist (0)", component.Find("h2").InnerText);
             Assert.Empty(component.FindAll("ul"));
@@ -49,32 +45,6 @@ namespace FlightFinder.Client.Test
                     Assert.Equal("Test Airways", item.QuerySelector(".airline").InnerText);
                     Assert.Equal("$457", item.QuerySelector(".price").InnerText);
                 });
-        }
-
-        [Fact]
-        public void TriggersRemoveFromShortlistCallback()
-        {
-            Itinerary itineraryToRemove = null;
-            var component = host.AddComponent<Shortlist>(new Dictionary<string, object>
-            {
-                [nameof(Shortlist.Itineraries)] = new List<Itinerary>
-                {
-                    TestItinerary.Create("Fake Airways", price: 123.45m),
-                    TestItinerary.Create("Test Airways", price: 456.78m),
-                },
-                [nameof(Shortlist.OnRemoveItinerary)] = EventCallback.Factory.Create<Itinerary>(this, itinerary =>
-                {
-                    itineraryToRemove = itinerary;
-                })
-            });
-
-            // Until we click a 'remove' button, no callback was fired
-            Assert.Null(itineraryToRemove);
-
-            // When we click 'remove' on a certain item, the callback fires for that item
-            component.Find("ul:nth-last-child(1) .close").Click();
-            Assert.NotNull(itineraryToRemove);
-            Assert.Equal("Test Airways", itineraryToRemove.AirlineName);
         }
     }
 }
