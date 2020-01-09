9e2d2165ee8343d21f78090c11e79d8513797a54
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-01-09 10:31:10 +0000

    Add SetTestCulture

---
+++ samples/aspnetcore/blazor/FlightFinder/FlightFinder.Client.Test/TestHelpers/SetTestCulture.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Threading;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+[assembly: Xunit.TestFramework("FlightFinder.Client.Test.TestHelpers.SetTestCulture", "FlightFinder.Client.Test")]
+
+namespace FlightFinder.Client.Test.TestHelpers
+{
+    public class SetTestCulture : XunitTestFramework
+    {
+        const string culture = "en-US";
+
+        public SetTestCulture(IMessageSink messageSink) : base(messageSink)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
+        }
+    }
+}
