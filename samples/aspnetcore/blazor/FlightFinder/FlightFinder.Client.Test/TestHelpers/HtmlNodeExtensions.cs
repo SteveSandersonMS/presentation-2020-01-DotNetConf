80d0d1c4796fbe2aa3b9aa0148ed5f3e9c943639
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-01-13 14:35:38 +0000

    Simplify GreysOutSearchResultsWhileLoading test

---
+++ samples/aspnetcore/blazor/FlightFinder/FlightFinder.Client.Test/TestHelpers/HtmlNodeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using HtmlAgilityPack;
+
+namespace FlightFinder.Client.Test.TestHelpers
+{
+    public static class HtmlNodeExtensions
+    {
+        public static bool HasClass(this HtmlNode node, string cssClassName)
+        {
+            return node.GetAttributeValue("class", string.Empty).Contains(cssClassName);
+        }
+    }
+}
